9bba6e52e418468845092e151d4a84ac1997d798
robert.lee <robert.lee@iisigroup.com>
2024-09-01 22:25:52 +0800

    adjust tests

--- chapter 5/api-catalog/src/test/java/com/apress/catalog/service/CountryServiceTest.java
+++ chapter 5/api-catalog/src/test/java/com/apress/catalog/service/CountryServiceTest.java
@@ -1,7 +1,13 @@
 package com.apress.catalog.service;
 
-import com.apress.catalog.dto.CountryDTO; 
- 
+import com.apress.catalog.dto.CountryDTO;
+import com.apress.catalog.dto.CurrencyDTO;
+import com.apress.catalog.mapper.ApiMapper;
+import com.apress.catalog.model.Country;
+import com.apress.catalog.model.Currency;
+import com.apress.catalog.repository.CountryRepository;
+import com.apress.catalog.repository.CurrencyRepository;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +16,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup; 
  
 
-import static org.junit.jupiter.api.Assertions.*; 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional; 
 
 /**
  * refer <p>
@@ -21,22 +29,28 @@ import static org.junit.jupiter.api.Assertions.*;
  * **/
 @SpringBootTest ( 
         properties = {   
-    		       "logging.level.org.springframework.jdbc.support.JdbcTransactionManager=TRACE",                         		       
-                   "logging.level.org.springframework.data=INFO",
-                   "logging.level.org.springframework.jdbc.core.JdbcTemplate=TRACE" , 
-                   "logging.level.org.springframework.transaction.interceptor=TRACE",
+//    		       "logging.level.org.springframework.jdbc.support.JdbcTransactionManager=TRACE",                         		       
+//                   "logging.level.org.springframework.data=INFO",
+//                   "logging.level.org.springframework.jdbc.core.JdbcTemplate=TRACE" , 
+//                   "logging.level.org.springframework.transaction.interceptor=TRACE",
 				   "logging.level.org.springframework.orm.jpa=DEBUG",
                    "logging.level.org.springframework.transaction=DEBUG",
         })
 //@TestPropertySource(properties = "debug=true")
 public class CountryServiceTest {  
+	@Autowired
+    private CountryRepository countryRepository;
+
+	@Autowired
+    private CurrencyRepository currencyRepository; 
     @Autowired
     private CountryService countryService;
     
 
     @SqlGroup({
 		@Sql(scripts = { "classpath:testcontainers/V1.0__init_database.sql" ,
-				         "classpath:testcontainers/V1.0.1__insert_rows_for_test.sql"   },  
+//				         "classpath:testcontainers/V1.0.1__insert_rows_for_test.sql"
+				},  
 				executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
 		@Sql(scripts = {"classpath:testcontainers/V1.0.2__remove_rows_for_test.sql"},
 				executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
@@ -44,24 +58,61 @@ public class CountryServiceTest {
 	@Test   
     @DisplayName("should perform a rollback because PersistenceException")
     public void should_rollback_transaction_on_exception() throws InterruptedException {    	
-    	CountryDTO countryDTO = countryService.getById(1L); 
+    	CountryDTO countryDTO = pseudocountryDTO () ; 
     	assertNotNull(countryDTO);
     	
 		assertThrows( Exception.class,				
-				() -> {
-					countryService.delete(1L);
-					CountryDTO countryDTO2 = countryService.getById(1L); 
-					countryService.save(countryDTO2);
-					},				
+				() -> countryService.save(countryDTO),				
 				"PersistenceException not thrown!");
         
       
         
-		var result = countryService.getById(1L) ;
-        assertAll( "state'enable was not updated to false" ,
-                () -> assertNotNull(result),
-                () -> assertNotEquals(true, result.getEnabled()),
-                () -> assertNotEquals(true, result.getStates().get(0).getEnabled())
-        ); 
+//		var result = countryService.getById(1L) ;
+//        assertAll( "state'enable was not updated to false" ,
+//                () -> assertNotNull(result),
+//                () -> assertNotEquals(true, result.getEnabled()),
+//                () -> assertNotEquals(true, result.getStates().get(0).getEnabled())
+//        ); 
+		 Optional<Country> result = countryRepository.findById(1L);     
+
+	        assertFalse(result.isPresent()); 
+        
     }
+    protected CountryDTO pseudocountryDTO () {
+    	CountryDTO countryDTO = new CountryDTO();
+        countryDTO.setEnabled(Boolean.TRUE);
+        countryDTO.setCode("ARG");
+        countryDTO.setName("Argentina");
+        countryDTO.setLocale("Tawain");
+        countryDTO.setTimezone("zh_TW");
+
+//        Currency currency = scenarioBy(1); 
+        Currency currency = scenarioBy(0);
+
+
+//        StateDTO stateDTO = new StateDTO(1L, "CA", "California", true, countryDTO);
+//        countryDTO.setStates(List.of(stateDTO));
+
+        CurrencyDTO currencyDTO = ApiMapper.INSTANCE.entityToDTO(currency );
+        countryDTO.setCurrency(currencyDTO); 
+        return countryDTO ;
+    } 
+    protected Currency scenarioBy(int scenario) {
+    	switch(scenario) {
+    		case 0:
+    			return dataIntegrityViolationCurrency() ;
+    		default:
+    			return currencyRepository.findById(1L).get() ;
+    	}
+    }
+    protected Currency dataIntegrityViolationCurrency() {
+    	Currency currency = new Currency( ); 
+        currency.setCode("ARS");
+        currency.setDescription("Peso argentino");
+        currency.setEnabled(true);
+        currency.setDecimalPlaces(2);
+        currency.setSymbol("$");  
+        return currency;
+    }
+    
 }
