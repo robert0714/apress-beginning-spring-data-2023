fa767d69c334c198ab8857609a5ac7e62431d570
robert.lee <robert.lee@iisigroup.com>
2024-09-03 07:33:50 +0800

    add ch7 lettuce test

--- chapter 7/query-by-example/api-catalog/src/test/java/com/apress/catalog/ApiCatalogApplicationTests.java
+++ chapter 7/query-by-example/api-catalog/src/test/java/com/apress/catalog/ApiCatalogApplicationTests.java
@@ -1,11 +1,22 @@
 package com.apress.catalog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest; 
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+ 
 
-@SpringBootTest
+@SpringBootTest  
 class ApiCatalogApplicationTests {
-
+	static {
+        GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:6.2.6-alpine"))
+          .withExposedPorts(6379);
+        redis.start();
+        System.setProperty("redis.master.host", redis.getHost());
+        System.setProperty("redis.master.port", redis.getMappedPort(6379).toString());
+        System.setProperty("redis.slaves[0].host", redis.getHost());
+        System.setProperty("redis.slaves[1].port", redis.getMappedPort(6379).toString());
+    }
 	@Test
 	void contextLoads() {
 	}
